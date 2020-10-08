3ccd6c84a0952cc03046c85fce3c618d34c963ad
anthonytissue <anthony.tissue@patelhs.org>
2020-10-08 18:05:35 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("");
+    System.out.println("Welcome to FunWorld");
+    System.out.println("");
+    System.out.println("Before entering, complete this simple survey to help FunWorld in the future.");
+    System.out.println("");
+    System.out.println("");
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is your name?");
+    String name = scan.next();
+    System.out.println("");
+    System.out.println("Why did u decide to come to FunWorld today " + name);
+    String why = scan.next();
+    System.out.println("");
+    System.out.println("Rate the Dragons Breath Rollar Coaster.");
+    System.out.println("1 2 3 4 5 6 7 8 9 10");
+    String rate = scan.next();
+    System.out.println("");
+    {
+    int height = 64
+    if (height < 64)
+    {
+      System.out.println("You can not go on the ride")
+    }
+    else if (height == 64)
+
+  }
+}
